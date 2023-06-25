444a86f2b296d00131712bf1ecbbeb9f19f1dd58
JacobBudda <123292739+JacobBudda@users.noreply.github.com>
2023-06-25 14:26:01 -0500

    Unit 3 submission

---
+++ Unit 3-1/models/players.js
@@ -0,0 +1,30 @@
+var mongoose = require("mongoose");
+
+var playersSchema = new mongoose.Schema({
+  Name: {
+    type: String,
+  },
+  Batting: {
+    type: String,
+  },
+  Bowling: {
+    type: String,
+  },
+  Runs: {
+    type: Number,
+  },
+  Wickets: {
+    type: Number,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Players = mongoose.model("Players", playersSchema);
+module.exports = Players;
